90867e140f2516decd7227f6078e4cb63edd9efc
NCrashed <NCrashed@gmail.com>
2013-12-11 23:02:27 +0400

    Creating project.

---
+++ src/Server/Main.hs
@@ -0,0 +1,19 @@
+-- Copyright 2013 Anton Gushcha
+--    This file is part of Gore&Ash.
+--
+--    Gore&Ash is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    Gore&Ash is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with Gore&Ash.  If not, see <http://www.gnu.org/licenses/>.
+module Main(main) where
+
+main::IO()
+main = putStrLn "Hello"
