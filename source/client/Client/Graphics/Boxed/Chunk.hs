d4a5ab94f42141583b28268aba5943121e7d1e7a
NCrashed <NCrashed@gmail.com>
2013-12-18 02:47:07 +0400

    Refactoring. I moved common graphics functions in separate module and removed duplicating functions for cube sides.

--- source/client/Client/Graphics/Boxed/Chunk.hs
+++ source/client/Client/Graphics/Boxed/Chunk.hs
@@ -23,40 +23,21 @@ import Data.Vec as Vec
 import Data.Monoid
 import Data.Word
 
+import Client.Graphics.Common
+import Client.Graphics.PolyCube
 import Game.Boxed.Chunk
 
 chunkFrameBuffer :: Texture2D RGBFormat -> BoxedChunk -> Float -> Vec2 Int -> FrameBuffer RGBFormat () ()
 chunkFrameBuffer atlas chunk angle size = paintSolid (litChunk atlas chunk angle size) emptyFrameBuffer
 
-paintSolid :: FragmentStream (Color RGBFormat (Fragment Float)) -> FrameBuffer RGBFormat () () -> FrameBuffer RGBFormat () ()
-paintSolid = paintColor NoBlending (RGB $ vec True)
-
-emptyFrameBuffer :: FrameBuffer RGBFormat () ()
-emptyFrameBuffer = newFrameBufferColor (RGB 0)
-
 litChunk :: Texture2D RGBFormat -> BoxedChunk -> Float -> Vec2 Int -> FragmentStream (Color RGBFormat (Fragment Float))
 litChunk atlas chunk angle size = fmap (enlight atlas) $ rasterizedChunk chunk angle size
-
-enlight :: Texture2D RGBFormat -> (Vec3 (Fragment Float), Vec2 (Fragment Float)) -> Color RGBFormat (Fragment Float)
-enlight tex (normv, uv) = RGB (c * vec (normv `dot` toGPU (0:.0:.1:.())))
-    where RGB c = sample (Sampler Linear Wrap) tex uv
     
 rasterizedChunk :: BoxedChunk -> Float -> Vec2 Int -> FragmentStream (Vec3 (Fragment Float), Vec2 (Fragment Float))
 rasterizedChunk chunk angle size = rasterizeFront $ transformedChunk chunk angle size
 
 transformedChunk :: BoxedChunk -> Float -> Vec2 Int -> PrimitiveStream Triangle (Vec4 (Vertex Float), (Vec3 (Vertex Float), Vec2 (Vertex Float)))
 transformedChunk chunk angle size = fmap (transform angle size) $ chunkTriangles chunk
-
-transform :: Float -> Vec2 Int -> (Vec3 (Vertex Float), Vec3 (Vertex Float), Vec2 (Vertex Float)) -> (Vec4 (Vertex Float), (Vec3 (Vertex Float), Vec2 (Vertex Float)))
-transform angle (width:.height:.()) (pos, normv, uv) = (transformedPos, (transformedNorm, uv))
-    where
-    modelMat = rotationVec (normalize (1:.0.5:.0.3:.())) angle `multmm` translation (-0.5)
-    viewMat  = translation (-(0:.0:.2:.()))
-    projMat  = perspective 1 100 (pi/3) (fromIntegral width / fromIntegral height)
-    viewProjMat = projMat `multmm` viewMat
-    transformedPos  = toGPU (viewProjMat `multmm` modelMat) `multmv` (homPoint pos :: Vec4 (Vertex Float))
-    transformedNorm = toGPU (mat4ToMat3 modelMat) `multmv` normv
-    mat4ToMat3 = Vec.map (Vec.take n3) . Vec.take n3
     
 chunkTriangles :: BoxedChunk -> PrimitiveStream Triangle (Vec3 (Vertex Float), Vec3 (Vertex Float), Vec2 (Vertex Float))
 chunkTriangles chunk = mconcat $ chunkFlatMapWithNeighbours genBorders chunk
@@ -70,8 +51,8 @@ chunkTriangles chunk = mconcat $ chunkFlatMapWithNeighbours genBorders chunk
             | val /= 0  = mempty
             | otherwise = fmap (sideTransform pos) side
             
-        neighboursGetters = [upNeighbour, downNeighbour, forwardNeighbour, rightNeigbour, backNeighbour, leftNeighbour]
-        neighboursSides   = [sidePosY,    sideNegY,      sidePosX,         sidePosZ,      sideNegX,      sideNegZ]
+        neighboursGetters = [upNeighbour,  downNeighbour, forwardNeighbour, rightNeigbour, backNeighbour, leftNeighbour]
+        neighboursSides   = [cubeSidePosY, cubeSideNegY,  cubeSidePosX,     cubeSidePosZ,  cubeSideNegX,  cubeSideNegZ]
         
         sideTransform :: Vec3 Int -> (Vec3 (Vertex Float), Vec3 (Vertex Float), Vec2 (Vertex Float)) -> (Vec3 (Vertex Float), Vec3 (Vertex Float), Vec2 (Vertex Float))
         sideTransform (cx:.cy:.cz:.()) (pos, normv, uv) = (Vec.take n3 newPos, normv, uv)
@@ -82,14 +63,4 @@ chunkTriangles chunk = mconcat $ chunkFlatMapWithNeighbours genBorders chunk
             chunkPos = fromList $ fmap fromIntegral [cx, cy, cz]
             transMat :: Mat44 (Vertex Float)
             transMat = toGPU $ scaling cellSize `multmm` translation chunkPos
-            newPos = transMat `multmv` (homPoint pos :: Vec4 (Vertex Float))
-        
-        sidePosX = toGPUStream TriangleStrip $ zip3 [1:.0:.0:.(), 1:.1:.0:.(), 1:.0:.1:.(), 1:.1:.1:.()] (repeat (1:.0:.0:.()))    uvCoords
-        sideNegX :: PrimitiveStream Triangle (Vec3 (Vertex Float), Vec3 (Vertex Float), Vec2 (Vertex Float))
-        sideNegX = toGPUStream TriangleStrip $ zip3 [0:.0:.1:.(), 0:.1:.1:.(), 0:.0:.0:.(), 0:.1:.0:.()] (repeat ((-1):.0:.0:.())) uvCoords
-        sidePosY = toGPUStream TriangleStrip $ zip3 [0:.1:.1:.(), 1:.1:.1:.(), 0:.1:.0:.(), 1:.1:.0:.()] (repeat (0:.1:.0:.()))    uvCoords
-        sideNegY = toGPUStream TriangleStrip $ zip3 [0:.0:.0:.(), 1:.0:.0:.(), 0:.0:.1:.(), 1:.0:.1:.()] (repeat (0:.(-1):.0:.())) uvCoords
-        sidePosZ = toGPUStream TriangleStrip $ zip3 [1:.0:.1:.(), 1:.1:.1:.(), 0:.0:.1:.(), 0:.1:.1:.()] (repeat (0:.0:.1:.()))    uvCoords
-        sideNegZ = toGPUStream TriangleStrip $ zip3 [0:.0:.0:.(), 0:.1:.0:.(), 1:.0:.0:.(), 1:.1:.0:.()] (repeat (0:.0:.(-1):.())) uvCoords
-        uvCoords = [0:.0:.(), 0:.1:.(), 1:.0:.(), 1:.1:.()]    
-        
+            newPos = transMat `multmv` (homPoint pos :: Vec4 (Vertex Float))      
---
+++ source/client/Client/Graphics/Common.hs
@@ -0,0 +1,45 @@
+-- Copyright 2013 Anton Gushcha
+--    This file is part of Gore&Ash.
+--
+--    Gore&Ash is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    Gore&Ash is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with Gore&Ash.  If not, see <http://www.gnu.org/licenses/>.
+module Client.Graphics.Common(
+      paintSolid
+    , emptyFrameBuffer
+    , enlight
+    , transform
+    ) where
+    
+import Graphics.GPipe
+import Data.Vec as Vec
+    
+paintSolid :: FragmentStream (Color RGBFormat (Fragment Float)) -> FrameBuffer RGBFormat () () -> FrameBuffer RGBFormat () ()
+paintSolid = paintColor NoBlending (RGB $ vec True)
+
+emptyFrameBuffer :: FrameBuffer RGBFormat () ()
+emptyFrameBuffer = newFrameBufferColor (RGB 0)
+    
+enlight :: Texture2D RGBFormat -> (Vec3 (Fragment Float), Vec2 (Fragment Float)) -> Color RGBFormat (Fragment Float)
+enlight tex (normv, uv) = RGB (c * vec (normv `dot` toGPU (0:.0:.1:.())))
+    where RGB c = sample (Sampler Linear Wrap) tex uv
+    
+transform :: Float -> Vec2 Int -> (Vec3 (Vertex Float), Vec3 (Vertex Float), Vec2 (Vertex Float)) -> (Vec4 (Vertex Float), (Vec3 (Vertex Float), Vec2 (Vertex Float)))
+transform angle (width:.height:.()) (pos, normv, uv) = (transformedPos, (transformedNorm, uv))
+    where
+    modelMat = rotationVec (normalize (1:.0.5:.0.3:.())) angle `multmm` translation (-0.5)
+    viewMat  = translation (-(0:.0:.2:.()))
+    projMat  = perspective 1 100 (pi/3) (fromIntegral width / fromIntegral height)
+    viewProjMat = projMat `multmm` viewMat
+    transformedPos  = toGPU (viewProjMat `multmm` modelMat) `multmv` (homPoint pos :: Vec4 (Vertex Float))
+    transformedNorm = toGPU (mat4ToMat3 modelMat) `multmv` normv
+    mat4ToMat3 = Vec.map (Vec.take n3) . Vec.take n3    
